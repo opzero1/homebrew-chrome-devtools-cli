e3955fc6d24c530852c479e08fc39e7c352e61a8
afif <afif.alauddin@reap.hk>
2026-04-22 06:42:18 +0800

    chore: sync published 0.1.2 asset checksum

--- Formula/chrome-devtools.rb
+++ Formula/chrome-devtools.rb
@@ -7,7 +7,7 @@ class ChromeDevtools < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/opzero1/chrome-devtools-cli/releases/download/v0.1.2/chrome-devtools-macos-arm64.zip"
-      sha256 "d96d78dd7401514bc609378085989b768426bcbe72d126f54cd94a3e428dd856"
+      sha256 "8d2c91fb07e01fa8915ae619a9b9fee2e9c41547f417dc8a9d7db4ca3eb707b5"
     end
   end
 
